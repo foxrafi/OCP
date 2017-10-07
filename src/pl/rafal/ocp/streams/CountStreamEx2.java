e44f5dec63054f199c39047d127d83e1a43d6b93
foxrafi <foxrafi@gmail.com>
2017-10-07 14:23:04 -0400

    Small refactoring

--- src/pl/rafal/ocp/streams/CountStreamEx2.java
+++ src/pl/rafal/ocp/streams/CountStreamEx2.java
@@ -9,9 +9,8 @@ public class CountStreamEx2 {
     public static void main(String[] args) {
         List<String> words = Arrays.asList("devout", "doyenne", "dainty", "decency","architect");
         Predicate<String> predicate = s-> s.startsWith("d");
-        long l = words.stream().filter(predicate).count();
-        System.out.println("Number of Matching Element:" + l);
-
+        long count = words.stream().filter(predicate).count();
+        System.out.println("Number of Matching Elements:" + count);
     }
 
 }
