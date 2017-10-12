6fe1fd495b158397994ffeca618fa99b6783dbcf
foxrafi <foxrafi@gmail.com>
2017-10-12 14:47:16 -0400

    Additional comments and lines were added to this simple example.

--- src/pl/rafal/ocp/streams/CountStreamEx1.java
+++ src/pl/rafal/ocp/streams/CountStreamEx1.java
@@ -5,11 +5,23 @@ import java.util.stream.Stream;
 public class CountStreamEx1 {
 
     public static void main(String[] args) {
-        Stream<String> s = Stream.of("a","b","c","d","e");
-        System.out.println(s.count());
 
+        pr("Let's create a stream containing 5 elements...");
+        Stream<String> s = Stream.of("a","b","c","d","e");
+
+        pr("Let's display the number of elements in this stream...");
+        pr("Size: " + s.count());      //it should be 5
+
+        pr("Let's assign a new 3-element stream to the reference variable used earlier to refer to the 5-element stream...");
         s = Stream.of("f","g","h");
-        System.out.println(s.count());
+
+        pr("Let's display the number of elements from the new stream...");
+        pr("Size: " + s.count());      //it should be 3
+
+    }
+
+    private static void pr(String str){
+        System.out.println(str);
     }
 
 }
